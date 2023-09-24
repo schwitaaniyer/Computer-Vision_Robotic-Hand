0a4b508d416d827bcdd311a20303e37c880413b9
Schwitaan Iyer <schwitaaniyer@gmail.com>
2023-09-24 14:55:11 +0530

    schwitaaniyer

---
+++ Robot_Hand.py
@@ -0,0 +1,19 @@
+import cv2
+import cvzone
+from cvzone.HandTrackingModule import HandDetector
+
+cap = cv2.VideoCapture(0)
+detector = cvzone.HandDetector(maxHands=1, detectionCon=0.7)
+mySerial = cvzone.SerialObject('COM4', 9600, 1)
+
+while True:
+    # cap = cv2.VideoCapture(0)
+    sucesss, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img)
+    if lmList:
+        fingers = detector.fingersUp()
+        print(fingers)
+        mySerial.sendData(fingers)
+    cv2.imshow('Image', img)
+    cv2.waitKey(1)
